3b8d66ffba4be32fd75232b3d99021d340e40bc4
David Diaz <seich@martianwabbit.com>
2018-05-05 00:33:01 +0000

    Re-indented some files I was missing.
    I think that's it.

--- bin/cli/base.js
+++ bin/cli/base.js
@@ -6,36 +6,36 @@ const { Command, flags } = require('@oclif/command');
 const Beau = require('../../src/beau');
 
 class Base extends Command {
-	openConfigFile(configFile) {
-		if (!fs.existsSync(configFile)) {
-			this.error(`The config file, ${configFile} was not found.`);
-			this.exit(1);
-		}
+    openConfigFile(configFile) {
+        if (!fs.existsSync(configFile)) {
+            this.error(`The config file, ${configFile} was not found.`);
+            this.exit(1);
+        }
 
-		return yaml.safeLoad(fs.readFileSync(configFile, 'utf-8'));
-	}
+        return yaml.safeLoad(fs.readFileSync(configFile, 'utf-8'));
+    }
 
-	loadConfig(configFile) {
-		const config = this.openConfigFile(configFile);
-		const env = dotenv.config().parsed || {};
+    loadConfig(configFile) {
+        const config = this.openConfigFile(configFile);
+        const env = dotenv.config().parsed || {};
 
-		return new Beau(config, env);
-	}
+        return new Beau(config, env);
+    }
 }
 
 Base.flags = {
-	config: flags.string({
-		char: 'c',
-		description: 'The configuration file to be used.',
-		default: 'beau.yml'
-	}),
-	verbose: flags.boolean({
-		char: 'V',
-		description: 'Show all additional information available for a command.'
-	}),
-	'no-format': flags.boolean({
-		description: `Disables color formatting for usage on external tools.`
-	})
+    config: flags.string({
+        char: 'c',
+        description: 'The configuration file to be used.',
+        default: 'beau.yml'
+    }),
+    verbose: flags.boolean({
+        char: 'V',
+        description: 'Show all additional information available for a command.'
+    }),
+    'no-format': flags.boolean({
+        description: `Disables color formatting for usage on external tools.`
+    })
 };
 
 module.exports = Base;
--- src/__mocks__/requireg.js
+++ src/__mocks__/requireg.js
@@ -1,15 +1,15 @@
 function requireg(name) {
-	return require(name);
+    return require(name);
 }
 
 requireg.std_resolving = false;
 
 requireg.resolve = function(name) {
-	if (requireg.std_resolving) {
-		return '';
-	} else {
-		throw new Error(`Failed to resolve.`);
-	}
+    if (requireg.std_resolving) {
+        return '';
+    } else {
+        throw new Error(`Failed to resolve.`);
+    }
 };
 
 module.exports = requireg;
