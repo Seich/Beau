c8ff4945d6af78e0985baebd16db94711ffc763a
Sergio Díaz <seich@martianwabbit.com>
2018-05-29 15:23:58 -0600

    Updated tests. (#30)
    With the introduction of data-driven tests in Jest 23 CLI tests could be made significantly smaller and easier to keep updated.

--- bin/cli/__tests__/request.spec.js
+++ bin/cli/__tests__/request.spec.js
@@ -18,33 +18,15 @@ describe('Request Command', () => {
 
 	afterEach(() => jest.restoreAllMocks());
 
-	it('Should request the given alias', async () => {
-		await RequestCommand.run(['anything']);
-		expect(result).toMatchSnapshot();
-	});
-
-	it('Should show all information available when being verbose', async () => {
-		await RequestCommand.run(['anything', '--verbose']);
-		expect(result).toMatchSnapshot();
-	});
-
-	it('Should output the response as json', async () => {
-		await RequestCommand.run(['anything', '--as-json']);
-		expect(result).toMatchSnapshot();
-	});
-
-	it('Should output the response as json verboselly', async () => {
-		await RequestCommand.run(['anything', '--as-json', '--verbose']);
-		expect(result).toMatchSnapshot();
-	});
-
-	it('Should output an unformatted version', async () => {
-		await RequestCommand.run(['anything', '--no-format']);
-		expect(result).toMatchSnapshot();
-	});
-
-	it('Should output nothing', async () => {
-		await RequestCommand.run(['anything', '--quiet']);
+	test.each([
+		['anything'],
+		['anything', '--verbose'],
+		['anything', '--as-json'],
+		['anything', '--as-json', '--verbose'],
+		['anything', '--no-format'],
+		['anything', '--quiet']
+	])('with flags:', async (...args) => {
+		await RequestCommand.run(args);
 		expect(result).toMatchSnapshot();
 	});
 
